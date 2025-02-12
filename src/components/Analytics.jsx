c5d4eb9aac1c82f9e0f97a7055df62fd1114bf51
kibeert <ckibertkoech@gmail.com>
2025-02-12 15:10:20 +0300

    `Refactor Analytics component layout and Navbar menu icon`

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -3,7 +3,7 @@ import laptop from "../assets/laptop.jpg"
 const Analytics = () => {
   return (
     <div className="w-full bg-white py-16 px-4'">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 p-5">
         <img className="w-[500px] mx-auto my-4 " src={laptop} alt="/" />
         <div className="flex flex-col justify-center">
             <p className="text-[#00df9a] font-bold " >DATA ANALYTICS DASHBOARD</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,11 +21,11 @@ const Navbar = () => {
         <li className="p-4" >Contact</li>
       </ul>
       <div className="sm:block md:hidden" onClick={handleNav} >
-        {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} /> }
+        {!nav ? <AiOutlineMenu size={20}/> : <AiOutlineClose size={20} /> }
         
       </div>
       <div className="md:hidden ease-out duration-1000 ">
-        <ul className= { nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[100%]' }>
+        <ul className= { nav ? 'fixed left-0 top-0 w-[70%] h-auto border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[100%]' }>
           <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4" >REACT</h1>
             <li className="p-4 border-b border-gray-600" >Home</li>
             <li className="p-4 border-b border-gray-600" >Company</li>
