51c6d4aebf655dabfbac4460f825155b3ce0c41f
kibeert <ckibertkoech@gmail.com>
2025-03-28 11:24:58 +0300

    Update Card component styles for improved visual consistency

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,8 @@ const Card = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white " >
         <div className="max-w-[1240px] mx-auto  grid md:grid-cols-3 gap-8 ">
-            <div className="w-full shadow-xl flex  flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300  ">
-                <img className="w-20 mx-auto mt-[-3rem] bg-white" src={single} alt="single" />
+            <div className="w-full shadow-xl bg-gray-200  flex  flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300  ">
+                <img className="w-20 mx-auto mt-[-3rem] bg-transparent" src={single} alt="single" />
                 <h2 className="text-2xl font-bold text-center py-8 " >Single User</h2>
                 <p className="text-center text-4xl  font-bold " >$129</p>
                 <div className="text-center font-medium" >
@@ -28,8 +28,8 @@ const Card = () => {
                 </div>
                 <button className="bg-[#00df9a] w-[200px] rounded-md font-medium mt-4 my-6 mx-auto px-6 py-3 text-black" >Start Trial</button>
             </div>
-            <div className="w-full shadow-xl  flex  flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300  ">
-                <img className="w-20 mx-auto mt-[-3rem] bg-white" src={triple} alt="triple" />
+            <div className="w-full shadow-xl bg-gray-200  flex  flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300  ">
+                <img className="w-20 mx-auto mt-[-3rem] bg-transpar" src={triple} alt="triple" />
                 <h2 className="text-2xl font-bold text-center py-8 " >Group Account</h2>
                 <p className="text-center text-4xl  font-bold " >$299</p>
                 <div className="text-center font-medium" >
